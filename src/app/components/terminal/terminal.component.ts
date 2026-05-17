ac5a41f56276a733048d648cd94e9335dbd7c185
Juan René Soler Sirí <juanr.soler_93@hotmail.es>
2026-05-17 23:01:55 +0000

    added: basic commands

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -1,47 +1,161 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { FitAddon } from '@xterm/addon-fit';
+import { Component, AfterViewInit } from '@angular/core';
 import { Terminal } from '@xterm/xterm';
+import { FitAddon } from '@xterm/addon-fit';
 
 @Component({
   selector: 'app-terminal',
-  //standalone: true,
+  imports: [],
   templateUrl: './terminal.component.html',
-  styleUrls: ['./terminal.component.css']
+  styleUrl: './terminal.component.css',
 })
 export class TerminalComponent implements AfterViewInit {
-
-  @ViewChild('terminalContainer', { static: true })
-  terminalContainer!: ElementRef;
 
   terminal!: Terminal;
   fitAddon!: FitAddon;
 
+  currentLine = '';
+  commandHistory: string[] = [];
+  historyIndex = -1;
+
+  languaje = 'en';
+
   ngAfterViewInit(): void {
+    this.fitAddon = new FitAddon();
 
     this.terminal = new Terminal({
       cursorBlink: true,
       theme: {
-        background: '#1e1e1e'
-      }
+        background: '#000000',
+        foreground: '#00ff00'
+      },
+      fontFamily: 'monospace',
+      fontSize: 15
     });
-
-    this.fitAddon = new FitAddon();
 
     this.terminal.loadAddon(this.fitAddon);
 
-    this.terminal.open(this.terminalContainer.nativeElement);
+    this.terminal.open(
+      document.getElementById('terminal-container')!
+    );
 
     this.fitAddon.fit();
+    this.showWelcomeMessage();
+    this.prompt();
+    this.listenTerminal();
+  }
 
-    this.terminal.writeln('Hola desde xterm.js en Angular');
-    this.terminal.write('$ ');
+  showWelcomeMessage() {
+    this.terminal.writeln('Juan Rene Soler Terminal v1.0');
+    this.terminal.writeln('');
+    this.terminal.writeln(
+      'Type "help" to see available commands.'
+    );
+    this.terminal.writeln('');
+  }
 
+  prompt() {
+    this.terminal.write('\r\n$ ');
+  }
+
+  listenTerminal() {
     this.terminal.onData((data) => {
-      this.terminal.write(data);
+      switch (data) {
+        case '\r':
+          this.handleCommand(this.currentLine.trim());
+          this.commandHistory.push(this.currentLine);
+          this.historyIndex =
+            this.commandHistory.length;
+          this.currentLine = '';
+          break;
+        case '\u007F':
+          if (this.currentLine.length > 0) {
+            this.currentLine =
+              this.currentLine.slice(0. - 1);
+            this.terminal.write('\b \b');
+          }
+          break;
+        default:
+          this.currentLine += data;
+          this.terminal.write(data);
+      }
     });
+  }
 
-    window.addEventListener('resize', () => {
-      this.fitAddon.fit();
-    });
+  handleCommand(command: string) {
+    const cmd = command.toLocaleLowerCase();
+    if (cmd === 'help') {
+      this.terminal.writeln(`
+Available commands:
+
+about
+whoami
+list projects
+list skills
+list educ
+resume
+download
+lang en
+lang es
+clear
+`);
+    }
+    else if (cmd === 'about') {
+      this.terminal.writeln(`
+Software developer passionate about backend,
+systems and interactive experiences.
+`);
+    }
+    else if (cmd === 'whoami') {
+      this.terminal.writeln('Juan Soler');
+    }
+    else if (cmd === 'list projects') {
+      this.terminal.writeln(`
+- Portfolio Terminal
+- Invoice System
+- Linux Server Automation
+`);
+    }
+    else if (cmd === 'list skills') {
+      this.terminal.writeln(`
+- AngularJS
+- C#
+- SQL
+- Linux
+- Docker
+`);
+    }
+    else if (cmd === 'download') {
+      window.open('/assets/cv.pdf');
+      this.terminal.writeln(`
+  Downloading PDF resume...
+`);
+    }
+    else if (cmd === 'download -w') {
+      window.open('/assets/cv.docx');
+      this.terminal.writeln(
+        'Downloading Word resume...'
+      );
+    }
+    else if (cmd === 'lang es') {
+      this.languaje = 'es';
+      this.terminal.writeln(
+        'Idioma cambiado a español'
+      );
+    }
+    else if (cmd === 'lang en') {
+      this.languaje = 'en';
+      this.terminal.writeln(
+        'Language changed to English'
+      );
+    }
+    else if (cmd === 'clear') {
+      this.terminal.clear();
+    }
+    else {
+      this.terminal.writeln(
+        `Command not found: ${command}`
+      )
+    }
+    this.prompt();
   }
 }
